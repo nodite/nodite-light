6fce03e4b6fba85a526d52572897400ca2e295ac
Oscaner Miao <oscaner1997@gmail.com>
2023-12-07 15:05:40 +0800

    chore(all): align env

--- websites/admin-web/src/types/env.d.ts
+++ websites/admin-web/src/types/env.d.ts
@@ -1,12 +1,7 @@
 declare interface ImportMeta {
   readonly env: {
     // Environment variables defined in .env
-    readonly VITE_UNSPLASH_ACCESS_KEY: string;
-    readonly VITE_GITHUB_CLIENT_ID: string;
     readonly VITE_API_BASE_URL: string;
-    readonly VITE_OPENAI_API_KEY: string;
-    readonly VITE_TTS_KEY: string;
-    readonly VITE_TTS_REGION: string;
   };
 }
 
