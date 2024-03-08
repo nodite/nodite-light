f704643907a49745b52f1b0bc09e486bf6fc0ca8
Oscaner Miao <oscaner1997@gmail.com>
2024-03-09 00:31:14 +0800

    fix: missing next for apiKey middleware

--- packages/admin-auth/src/middlewares/apiKey.middleware.ts
+++ packages/admin-auth/src/middlewares/apiKey.middleware.ts
@@ -5,14 +5,18 @@ import httpStatus from 'http-status';
 const apiKey = (req: Request, res: Response, next: NextFunction) => {
   let xApiKey: string | undefined;
   const token: string = config.xApiKey;
+
   if (req.header('x-api-key')) {
     xApiKey = req.header('x-api-key')?.trim();
   }
+
   if (!!token && xApiKey === token.trim()) {
     return next();
   }
+
   logger.error('Missing x-api-key in request header or it does not match with env variable');
-  throw new AppError(httpStatus.UNAUTHORIZED, 'Access forbidden: invalid x-api-key');
+
+  return next(new AppError(httpStatus.UNAUTHORIZED, 'Access forbidden: invalid x-api-key'));
 };
 
 export default apiKey;
